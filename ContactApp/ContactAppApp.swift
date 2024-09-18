f4fdb5c969487efbe9f4b488b63b7bbe7973c65d
Trine H. Davidsen <114672051+THD-94@users.noreply.github.com>
2024-09-18 14:11:05 +0200

    Initial Commit

---
+++ ContactApp/ContactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppApp.swift
+//  ContactApp
+//
+//  Created by Trine Davidsen on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
